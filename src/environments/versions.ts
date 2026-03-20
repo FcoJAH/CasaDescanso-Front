c0c0c08ad6d044944868a73923030b8d37d2bbec
Francisco Alcala <francisco.alcala@hotmail.com>
2026-03-20 17:21:59 -0600

    [FEAT] Modulo nuevo para expediente de residentes.

--- src/environments/versions.ts
+++ src/environments/versions.ts
@@ -1,7 +1,7 @@
 export const APP_VERSION = {
   major: 1, // Features grandes / Cambios estructurales
-  minor: 1, // Features pequeñas / Nuevas funcionalidades
-  patch: 3, // Defectos (Bugs) / Mejoras menores
+  minor: 3, // Features pequeñas / Nuevas funcionalidades
+  patch: 0, // Defectos (Bugs) / Mejoras menores
   get full() {
     return `${this.major}.${this.minor}.${this.patch}`;
   }
