3a18c00bf4320b3def4efe667d7af4feb02ba20e
Francisco Alcala <alcalaherrerafcoj@gmail.com>
2026-04-08 03:38:40 -0600

    [FEAT] Modificación para ver nombre de incidentes del día

--- src/app/pages/dashboard/dashboard.service.ts
+++ src/app/pages/dashboard/dashboard.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environments';
 
-// Definimos la interfaz según tu JSON para tener tipado estricto
 export interface DashboardData {
   totalResidents: number;
   activeResidents: number;
@@ -18,6 +17,13 @@ export interface DashboardData {
   activeWorkersNames: string[];
 }
 
+// Corregimos la interfaz: El backend manda un string único por cada registro
+export interface IncidentToday {
+  id: number;
+  residentId: number;
+  residentName: string; // Es un string, no un array
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -25,8 +31,14 @@ export class DashboardService {
   private http = inject(HttpClient);
   private myAppUrl = environment.apiUrl;
   private apiUrl = `${this.myAppUrl}/Dashboard`;
+  private apiIncidents = `${this.myAppUrl}/Incidents/today`;
 
   getStats(): Observable<DashboardData> {
     return this.http.get<DashboardData>(this.apiUrl);
+  }
+
+  // Corregido: Usamos paréntesis () y definimos que retorna un Array []
+  getTodayIncidents(): Observable<IncidentToday[]> {
+    return this.http.get<IncidentToday[]>(this.apiIncidents);
   }
 }
