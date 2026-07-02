42f739cab65bdca9bb24f988e5e66b139d09f662
Francisco Alcala <alcalaherrerafcoj@gmail.com>
2026-07-02 13:14:52 -0600

    Ajuste por nueva versión

--- src/environments/versions.ts
+++ src/environments/versions.ts
@@ -1,6 +1,6 @@
 export const APP_VERSION = {
   major: 2, // Features grandes / Cambios estructurales
-  minor: 2, // Features pequeñas / Nuevas funcionalidades
+  minor: 6, // Features pequeñas / Nuevas funcionalidades
   patch: 0, // Defectos (Bugs) / Mejoras menores
   get full() {
     return `${this.major}.${this.minor}.${this.patch}`;
