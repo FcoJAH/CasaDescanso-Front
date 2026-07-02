48e3981fee3a6bc92dd5072a5ca97c42d4f1e06a
Francisco Alcala <alcalaherrerafcoj@gmail.com>
2026-07-02 16:07:31 -0600

    FEAT: Agregar logica de JWT y auth interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,17 @@ import { ApplicationConfig, provideZoneChangeDetection, ErrorHandler } from '@an
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { GlobalErrorHandler } from './utils/global-error-handler';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([authInterceptor])
+    ),
     { provide: ErrorHandler, useClass: GlobalErrorHandler }
   ]
 };
